a26eae11f3bb144c801d4ea951b649ce50075f68
SashaYaroshevich <Pristigesasha@gmail.com>
2022-06-08 20:16:16 +0300

    creating an icon and GUI

--- prog.py
+++ prog.py
@@ -1,63 +1,89 @@
-print("""
-    <-1-ЦА-->
-    <-2-Брест-->
-	<-3-Пинск-->
-	<-4-Гродно-->
-	<-5-Барановичи-->
-	<-6-Минск1-->
-	<-7-Минск2-->
-	<-8-Солигорск-->
-	<-9-Новополоцк-->
-	<-10-Гомель-->
-	<-11-могилёв-->
-	<-12--Мозырь-->
-	<-13--Витебск--> """)
+from tkinter import *
+
+class window:
+	def __init__(self, width, height, title="MyWindow", resizable=(False, False), icon="D:\\work\cer_prog\solving-local-problems-0\psc.ico"):
+		self.root = Tk()
+		self.root.title(title)
+		self.root.geometry(f"{width}x{height}+200+200")
+		self.root.resizable(resizable[0], resizable[1])
+		if icon:
+			self.root.iconbitmap(icon)
+	
+	def run(self):
+		self.root.mainloop()
+
+if __name__ == "__main__":
+	window = window(500, 500, "Change PS")
+	window.run()
+
+# root = Tk()
+# root.title("branch change")
+# root.geometry("400x400+200+300")
+# root.resizable(False, False)
+# root.iconbitmap("D:\\work\cer_prog\solving-local-problems-0\psc.ico")
+
+
+# root.mainloop()
+# print("""
+#     <-1-ЦА-->
+#     <-2-Брест-->
+# 	<-3-Пинск-->
+# 	<-4-Гродно-->
+# 	<-5-Барановичи-->
+# 	<-6-Минск1-->
+# 	<-7-Минск2-->
+# 	<-8-Солигорск-->
+# 	<-9-Новополоцк-->
+# 	<-10-Гомель-->
+# 	<-11-могилёв-->
+# 	<-12--Мозырь-->
+# 	<-13--Витебск--> """)
     
-your_choice = int(input('Введите нужный филиал для переключения цифрой выше: '))
+# your_choice = int(input('Введите нужный филиал для переключения цифрой выше: '))
 
-spisok_dan = {1: '<connect1 isInet="1" server="192.168.29.105" port="1792"/><!--ЦА-->',
-2: '<connect1 isInet="1" server="192.168.29.104" port="1782"/><!--Брест-->',
-3: '<connect1 isInet="1" server="192.168.29.104" port="1789"/><!--Пинск-->',
-4: '<connect1 isInet="1" server="192.168.29.104" port="1780"/><!--Гродно-->',
-5: '<connect1 isInet="1" server="192.168.29.104" port="1786"/><!--Барановичи-->',
-6: '<connect1 isInet="1" server="192.168.29.103" port="1781"/><!--Минск1-->',
-7: '<connect1 isInet="1" server="192.168.29.103" port="1791"/><!--Минск2-->',
-8: '<connect1 isInet="1" server="192.168.29.103" port="1788"/><!--Солигорск-->',
-9: '<connect1 isInet="1" server="192.168.29.103" port="1787"/><!--Новополоцк-->',
-10: '<connect1 isInet="1" server="192.168.29.122" port="1785"/><!--Гомель-->',
-11: '<connect1 isInet="1" server="192.168.29.122" port="1783"/><!--могилёв-->',
-12: '<connect1 isInet="1" server="192.168.29.122" port="1784"/><!--Мозырь-->',
-13: '<connect1 isInet="1" server="192.168.29.122" port="1790"/><!--Витебск-->'
-    }
+# spisok_dan = {1: '<connect1 isInet="1" server="192.168.29.105" port="1792"/><!--ЦА-->',
+# 2: '<connect1 isInet="1" server="192.168.29.104" port="1782"/><!--Брест-->',
+# 3: '<connect1 isInet="1" server="192.168.29.104" port="1789"/><!--Пинск-->',
+# 4: '<connect1 isInet="1" server="192.168.29.104" port="1780"/><!--Гродно-->',
+# 5: '<connect1 isInet="1" server="192.168.29.104" port="1786"/><!--Барановичи-->',
+# 6: '<connect1 isInet="1" server="192.168.29.103" port="1781"/><!--Минск1-->',
+# 7: '<connect1 isInet="1" server="192.168.29.103" port="1791"/><!--Минск2-->',
+# 8: '<connect1 isInet="1" server="192.168.29.103" port="1788"/><!--Солигорск-->',
+# 9: '<connect1 isInet="1" server="192.168.29.103" port="1787"/><!--Новополоцк-->',
+# 10: '<connect1 isInet="1" server="192.168.29.122" port="1785"/><!--Гомель-->',
+# 11: '<connect1 isInet="1" server="192.168.29.122" port="1783"/><!--могилёв-->',
+# 12: '<connect1 isInet="1" server="192.168.29.122" port="1784"/><!--Мозырь-->',
+# 13: '<connect1 isInet="1" server="192.168.29.122" port="1790"/><!--Витебск-->'
+#     }
 
     
-with open("D:\\work\connect.xml", "r", encoding="utf-8") as file1:
-	new_file = ''
-	for line in file1:
-		if 'connect1' not in line:
-			new_str = line.replace("connect","connect1")
-			new_file += new_str
-		else:
-			new_file += line
+# with open("D:\\work\connect.xml", "r", encoding="utf-8") as file1:
+# 	new_file = ''
+# 	for line in file1:
+# 		if 'connect1' not in line:
+# 			new_str = line.replace("connect","connect1")
+# 			new_file += new_str
+# 		else:
+# 			new_file += line
 
 
-with open("D:\\work\connect.xml", "w", encoding="utf-8") as file2:
-	file2.write(new_file)	
+# with open("D:\\work\connect.xml", "w", encoding="utf-8") as file2:
+# 	file2.write(new_file)	
 
 
-choise = spisok_dan[your_choice]
+# choise = spisok_dan[your_choice]
 
-with open("D:\\work\connect.xml", "r", encoding="utf-8") as file3:
-	new_file2 = ''
-	for line in file3:
-		if choise in line:
-			new_str = line.replace("connect1","connect")
-			new_file2 += new_str
-		else:
-			new_file2 += line
+# with open("D:\\work\connect.xml", "r", encoding="utf-8") as file3:
+# 	new_file2 = ''
+# 	for line in file3:
+# 		if choise in line:
+# 			new_str = line.replace("connect1","connect")
+# 			new_file2 += new_str
+# 		else:
+# 			new_file2 += line
 
-with open("D:\\work\connect.xml", "w", encoding="utf-8") as file4:
-	file4.write(new_file2)
+# with open("D:\\work\connect.xml", "w", encoding="utf-8") as file4:
+# 	file4.write(new_file2)
 
-print(f'Готово, Переключено на {your_choice}, работайте)))')
-exit_program = input('Нажмите Enter для выхода')
+# print(f'Готово, Переключено на {your_choice}, работайте)))')
+# exit_program = input('Нажмите Enter для выхода')
