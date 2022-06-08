515011763ef0ac93517a2c0f464649cedd0a3427
SashaYaroshevich <Pristigesasha@gmail.com>
2022-06-08 21:14:25 +0300

    new window

--- prog.py
+++ prog.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from child_window import ChildWindow
+
 
 class window:
 	def __init__(self, width, height, title="MyWindow", resizable=(False, False), icon="D:\\work\cer_prog\solving-local-problems-0\psc.ico"):
@@ -12,18 +14,18 @@ class window:
 	def run(self):
 		self.root.mainloop()
 
+	def create_child(self, width, height, title="Child", resizable=(False, False), icon=None):
+		ChildWindow(self.root, width, height, title, resizable, icon)
+
+
 if __name__ == "__main__":
 	window = window(500, 500, "Change PS")
+	window.create_child(200, 100)
+
+	
 	window.run()
 
-# root = Tk()
-# root.title("branch change")
-# root.geometry("400x400+200+300")
-# root.resizable(False, False)
-# root.iconbitmap("D:\\work\cer_prog\solving-local-problems-0\psc.ico")
 
-
-# root.mainloop()
 # print("""
 #     <-1-ЦА-->
 #     <-2-Брест-->
